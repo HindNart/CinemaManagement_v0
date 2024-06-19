3b333a1fc557ca0f38f73c5329a951f6d1577347
Kato-30 <trandinhan19112003@gmail.com>
2024-06-19 16:05:19 +0700

    cmt

--- src/main/java/com/Group3/ManagementCinema/controller/adminController.java
+++ src/main/java/com/Group3/ManagementCinema/controller/adminController.java
@@ -14,32 +14,43 @@ import com.Group3.ManagementCinema.entity.Customer;
 import com.Group3.ManagementCinema.service.CinemaRoomService;
 import com.Group3.ManagementCinema.service.CustomerService;
 import com.Group3.ManagementCinema.service.EmployeeService;
+import com.Group3.ManagementCinema.service.MovieService;
 
 @Controller
 @RequestMapping("/admin")
 public class adminController {
     @Autowired
     private CustomerService customerService;
+    @Autowired
     private EmployeeService employeeService;
+    @Autowired
     private CinemaRoomService cinemaroomService;
+    @Autowired
+    private MovieService movieService;
 
     // Hiển thị danh sách khách hàng
     @GetMapping("/adCustomer.html")
     public String showCustomerList(Model model) {
-        model.addAttribute("listCustomer", customerService.getAllCustomers());
+        model.addAttribute("listCustomers", customerService.getAllCustomers());
         return "admin/adCustomer"; // Trả về template adCustomer.html trong thư mục templates/admin/
     }
     
     @GetMapping("/adEmployee.html")
     public String showEmployeeList(Model model) {
         model.addAttribute("listEmployees", employeeService.getAllEmployees());
-        return "admin/adEmployee"; // Trả về template adCustomer.html trong thư mục templates/admin/
+        return "admin/adEmployee"; // Trả về template adEmployee.html trong thư mục templates/admin/
+    }
+    
+    @GetMapping("/adMovie.html")
+    public String showMovieList(Model model) {
+        model.addAttribute("listMovies", movieService.getAllMovies());
+        return "admin/adMovie"; // Trả về template adMovie.html trong thư mục templates/admin/
     }
     
     @GetMapping("/adRoom.html")
     public String showRoomList(Model model) {
-        model.addAttribute("listCinemaRoom", cinemaroomService.getAllCinemaRooms());
-        return "admin/adRoom"; // Trả về template adCustomer.html trong thư mục templates/admin/
+        model.addAttribute("listCinemaRooms", cinemaroomService.getAllCinemaRooms());
+        return "admin/adRoom"; // Trả về template adRoom.html trong thư mục templates/admin/
     }
 }
 
